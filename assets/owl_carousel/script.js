7640ade46098ce439ccd5cf15f574f36d0a83601
NG SHEN MENG <shenmeng0124@gmail.com>
2021-09-08 01:04:00 +0800

    first commit

---
+++ assets/owl_carousel/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    
+    $(".owl-carousel").owlCarousel({
+
+        dots: true,
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items: 1,
+            },
+            768:{
+                items: 2,
+            },
+            1100:{
+                items: 3,
+            },
+            1400:{
+                items: 4,
+                loop: false,
+            }
+        }
+    });
+});
